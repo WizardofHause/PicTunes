13965dd5c540237950b577fb3d95dd03380481fd
Jon Hause <jonrhause@gmail.com>
2022-10-05 23:55:12 -0600

    Created one-click functionality for wishlist add, made popularity score dynamic

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ let albumData;
 let currentAlbum;
 
 fetch("http://localhost:3000/albums")
-.then(response => response.json())
-.then(json => {
-  albumData = json;
-  albumData.forEach(album => {
-    makeIcons(album)
-  })
-  displayData(albumData[0])
-  engageWantButton();
-});
+  .then(response => response.json())
+  .then(json => {
+    albumData = json;
+    albumData.forEach(album => {
+      makeIcons(album)
+    })
+    displayData(albumData[0])
+    engageWishButton()
+    engagePopularityForm()
+    engageResetButton()
+  });
 
 function makeIcons(album) {
   let albumContainer = document.querySelector('#img-container')
@@ -27,34 +29,35 @@ function makeIcons(album) {
   })
 };
 
-function displayData(album){
+function displayData(album) {
   currentAlbum = album;
-  
+
   let albumTitle = document.querySelector('#album-title');
   let albumImg = document.querySelector('#main-display');
   let artistName = document.querySelector('#artist-name');
   let releaseDate = document.querySelector('#release-date');
   let trackList = document.querySelector('#track-list');
-  let popularityScore = document.querySelector('#popularity-score')
-  let pageBody = document.getElementsByTagName('body')
+  let popularityScore = document.querySelector('#score')
+  let wishButton = document.querySelector('#wish-button')
 
-  albumTitle.textContent = `${album.title} `;
+  albumTitle.textContent = `${album.title}`;
   albumImg.src = album.img_url;
   artistName.textContent = `Artist: ${album.artist}`;
   releaseDate.textContent = `Released: ${album.date.month} ${album.date.day}, ${album.date.year}`;
   trackList = getTrackData(album.tracks);
-  popularityScore.textContent = `Popularity Score: ${album.popularity}/100`;
-  pageBody.style[background-image] = "linear-gradient(`${album.color}`"
+  popularityScore.textContent = `${album.popularity}`;
+  wishButton.textContent = album.wish_list ? 'ADDED!' : 'ADD TO WISHLIST';
 }
 
 //ITERATES THROUGH ALBUM TRACKS, RETURNS THE TRACK NAME
-function getTrackData(tracks){
-  for(const track of tracks){
+function getTrackData(tracks) {
+  for (const track of tracks) {
     makeListItem(track)
   };
 }
+
 // MAKES LIST ELEMENTS AND ADDS THEM TO THE TRACK LIST UL, WITH CLASS .TRACK
-function makeListItem(track){
+function makeListItem(track) {
   const listItem = document.createElement('li')
   listItem.textContent = track
   listItem.classList.add("track")
@@ -62,19 +65,59 @@ function makeListItem(track){
   const trackList = document.querySelector('#track-list')
   trackList.appendChild(listItem);
 }
-//MAKE AN EVENT LISTENER ON THE MAIN DISPLAY IMAGE THAT ADDS IMAGES TO A FAVORITES LIST
-function engageWantButton() {
-  let wantButton = document.querySelector('#want-that');
-  let favoriteBar = document.querySelector('#favorites-bar');
-  let albumTitle = document.querySelector('#album-title');
 
-  wantButton.addEventListener('click', () => {
-    let wanted = document.createElement('p')
-    wanted.classList.add('wanted')
-    wanted.textContent = (albumTitle.textContent);
-    favoriteBar.appendChild(wanted)
+//EVENT LISTENER FOR THE WISHLIST BUTTON; INVOKES ONE OF TWO CALLBACK FUNCTIONS
+function engageWishButton() {
+  let wishButton = document.querySelector('#wish-button');
+  wishButton.addEventListener('click', (event) => {
+    currentAlbum.wish_list = !currentAlbum.wish_list;
+    event.target.textContent === 'ADD TO WISHLIST' ? createWishedItem() : alertAlreadyWished();
+    event.target.textContent = 'ADDED!'
   })
 }
+
+//MAKES A <P> ELEMENT, GIVES IT CLASS .WISHED, ALBUMTITLE CONTENT, AND ADDS IT TO THE WISHLIST DIV
+function createWishedItem() {
+  let wishList = document.querySelector('#wish-list')
+  let albumTitle = document.querySelector('#album-title')
+
+  let wishedAlbum = document.createElement('li')
+  wishedAlbum.classList.add('wished')
+  wishedAlbum.textContent = (albumTitle.textContent)
+  wishList.appendChild(wishedAlbum)
+}
+
+//ALERTS THAT THE ALBUM IS ALREADY ADDED
+function alertAlreadyWished() {
+  let albumTitle = document.querySelector('#album-title')
+  alert(`(☞ﾟヮﾟ)☞ ${albumTitle.textContent} is already on your wishlist! ☜(ﾟヮﾟ☜)`);
+  currentAlbum.wish_list = true;
+}
+
+function engagePopularityForm() {
+  let popularityForm = document.querySelector('#popularity-form');
+  popularityForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const scoreAdd = event.target['pop-score'].value;
+    currentAlbum.popularity += parseInt(scoreAdd);
+    document.querySelector('#score').textContent = currentAlbum.popularity;
+    popularityForm.reset();
+  })
+}
+
+function engageResetButton() {
+  const resetButton = document.querySelector('#reset-button');
+  resetButton.addEventListener('click', () => {
+    currentAlbum.popularity = 0
+    document.querySelector('#score').textContent = 0
+  })
+}
+
+//CREATE A 'LISTED' BOOLEAN ATTRIBUTE IN THE ALBUMS OBJECTS TO CHECK FOR WISHLIST
+//WRITE CSS TO COLLECT THE ALBUM ART INTO ONE CLEAN CONTAINER...PAGE BOOK?
+//MAKE POPULARITY SCORE ADJUSTABLE
+//
 
 //Blank format to add extra stuff to db.json
 // {
